196c6e05c3cde32b950ee291ae9fae3f0ebc9a6e
Anton HUBA <ahuba@e1r1p5.unit.ua>
2018-02-02 21:58:12 +0200

    everything is done

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahuba <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/16 15:00:52 by ahuba             #+#    #+#             */
+/*   Updated: 2018/02/02 09:58:55 by ahuba            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "./libft/libft.h"
+# include <fcntl.h>
+
+# define BUFF_SIZE 15
+
+int		get_next_line(const int fd, char **line);
+
+#endif
